3c32876bd516040c68ca63d67978cb0574114a17
sykhronics <sykhronics@localhost>
2013-06-02 21:46:01 -0600

    VM works.

--- run.sh
+++ run.sh
@@ -1,7 +1,44 @@
-#!/bin/bash
+#!/bin/sh
+# run.sh - Run the currently built game
 
-SQ=src/External/Squirrel/bin/sq.exe
+CONFIGDIR=Config
+OUTPUTDIR=output
 
-$SQ src/Common/startup.nut
+# Are we running Windows? #
+if [ "$WINDIR" != "" ]; then
+	SYSTEM_SUFFIX=
+	EXT=exe
+else
+	SYSTEM_SUFFIX=_`uname -s`
+	EXT=`uname -m`
+fi
+
+
+if [ ! -e "$CONFIGDIR/.project" ]; then
+	echo "ERROR: File \".project\" does not exist!  Please run \"setup.sh\" to generate."
+	exit 1
+fi
+
+if [ ! -e "$CONFIGDIR/.sku" ]; then
+	echo "ERROR: File \".sku\" does not exist!  Please run \"setup.sh\" to generate."
+	exit 1
+fi
+
+if [ ! -e "$CONFIGDIR/.target$SYSTEM_SUFFIX" ]; then
+	echo "ERROR: File \".target$SYSTEM_SUFFIX\" does not exist!  Please run \"setup.sh\" to generate."
+	exit 1
+fi
+
+PROJECT=`cat $CONFIGDIR/.project`
+
+SKU=`cat $CONFIGDIR/.sku`
+SKU_NAME=`basename $SKU`
+
+TARGET=`cat $CONFIGDIR/.target$SYSTEM_SUFFIX`
+TARGET_NAME=`basename $TARGET`
+
+TARGET_DIR=$OUTPUTDIR/$SKU_NAME-$TARGET_NAME
+
+./$TARGET_DIR/$PROJECT.$EXT -DIR ./ -SAVE $TARGET_DIR/ $@
 
 exit 0
--- src/Quack/App.cpp
+++ src/Quack/App.cpp
@@ -4,10 +4,11 @@
 #include <Search/Search.h>
 #include <Render/Render.h>
 
-//#include <spine-gel/spine.h>
-
 #include "App.h"
-//#include "Puck/ProjectPuck.h"
+// - ------------------------------------------------------------------------------------------ - //
+#include <squirrel.h> 
+#include <sqstdio.h> 
+#include <sqstdaux.h> 
 // - ------------------------------------------------------------------------------------------ - //
 using namespace Texture;
 using namespace Render;
@@ -15,29 +16,40 @@ using namespace Render;
 //TextureHandle Texas;
 extern cFont* Font;
 cFont* Font;
+
+DataBlock* NutFile;
 //cRenderTarget* RT_Main;
-//cRenderTarget* RT_Blur[2];
-//
-//cUberShader* PPShader;
 //cUberShader* BlurShader;
-//
-//float animationTime = 0;
-//spine::Atlas* atlas = 0;
-//spine::SkeletonData* skeletonData = 0;
-//spine::Animation* animation = 0;
-//spine::Skeleton* skeleton = 0;
+// - ------------------------------------------------------------------------------------------ - //
+void PrintFunc(HSQUIRRELVM v, const SQChar *s, ...) {
+	char Dummy[4096];
+	va_list arglist; 
+	va_start(arglist, s); 
+	vsprintf(Dummy, s, arglist);
+	Log(Dummy);
+	va_end(arglist); 
+} 
+// - ------------------------------------------------------------------------------------------ - //
+void ErrorFunc(HSQUIRRELVM v, const SQChar *s, ...) {
+	char Dummy[4096];
+	va_list arglist; 
+	va_start(arglist, s); 
+	vsprintf(Dummy, s, arglist);
+	Log(Dummy);
+	va_end(arglist); 
+} 
 // - ------------------------------------------------------------------------------------------ - //
 cApp::cApp() {
-	Search::AddDirectory( "Project/" );
+	Search::AddDirectory( "project/" );
 	
 	Font = new cFont( Search::Search( "C64Pro.fnt" ) );
-/*
+
 	// TODO: On some sort of resize event, trigger a delete and recreation of the RenderTargets.		
 	int Width = Screen::Native[0].GetWidth();
 	int Height = Screen::Native[0].GetHeight();
 //	int Width = Screen::Native[0].GetActualWidth();
 //	int Height = Screen::Native[0].GetActualHeight();
-
+/*
 	RT_Main = new cRenderTarget( Width, Height, 1,0,0 );
 	RT_Blur[0] = new cRenderTarget( Width>>2, Height>>2, 1,0,0 );
 	RT_Blur[1] = new cRenderTarget( Width>>2, Height>>2, 1,0,0 );
@@ -75,17 +87,38 @@ cApp::cApp() {
 	}
 
 	Project = new cProjectPuck();*/
+	
+	vm = sq_open(1024);	// Stack Size //
+	sq_setprintfunc( vm, PrintFunc, ErrorFunc ); //sets the print function
+	sqstd_seterrorhandlers(vm);	
+
+	sq_pushroottable(vm); //push the root table(were the globals of the script will be stored)
+	
+	NutFile = new_read_DataBlock( Search::Search("main.nut") );
+		
+	if( SQ_SUCCEEDED(sq_compilebuffer(vm, NutFile->Data, NutFile->Size, "main.nut", true)) ) {
+		sq_push(vm,-2);
+		if ( SQ_SUCCEEDED(sq_call(vm,1,false,SQTrue)) ) {
+			// Success, and closure removed //
+		}
+		else {
+			sq_pop(vm,1); // removes the closure
+		}
+	}
+
+	//Log("* Nut: %s", Search::Search("main.nut") );
 }
 // - ------------------------------------------------------------------------------------------ - //
 cApp::~cApp() {
-//	delete Project;
-//
-//	// Cleanup //
+	delete_DataBlock( NutFile );
+	
+	// Cleanup //
+	sq_pop(vm,1); //pops the root table
+
+	sq_close(vm);
+	
+	
 //	delete BlurShader;
-//	delete PPShader;
-//	
-//	delete RT_Blur[0];
-//	delete RT_Blur[1];
 //	delete RT_Main;
 	
 	delete Font;
@@ -95,143 +128,15 @@ cApp::~cApp() {
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
-#ifdef PRODUCT_CLIENT
-// - ------------------------------------------------------------------------------------------ - //
 void cApp::Step( ) {
 //	Project->Step();
 }
 // - ------------------------------------------------------------------------------------------ - //
 void cApp::Draw( Screen::cNative& Native ) {	
-/*	static const float Verts[] = {
-		-1,-1,0,
-		-1,+1,0,
-		+1,-1,0,
-		+1,+1,0,
-	};
-
-	static const UVType UVs[] = {
-		UV_ZERO,UV_ZERO,
-		UV_ZERO,UV_ONE,
-		UV_ONE,	UV_ZERO,
-		UV_ONE,	UV_ONE,
-	};
-
-	// ******* //
-
-	RT_Main->Bind();
-
-	// Clear BG with Noise //	
-	{
-		Default->Bind( NoiseShader );
-		Default->UniformMatrix4x4( 0, Matrix4x4::Identity );
-		Default->UniformColor( 1, GEL_RGB(96,96,96) ); // GlobalColor //
-		static float SeedHack = 0;
-		SeedHack += 0.01f;
-		if ( SeedHack > 1.0f )
-			SeedHack = 0.0f;
-		Default->Uniform1f( 3, SeedHack ); // Seed //
-		Default->BindUniforms();
-		Default->Attrib( 0, Verts );
-		Default->Attrib( 1, UVs );
-		Default->DrawArrays( GEL_TRIANGLE_STRIP, 4 );		
-	}
-*/	
 	Matrix4x4 Matrix = Matrix4x4::Identity;
 	Matrix(0,0) = 1.0f / (float)Native.GetWidth();//1.0f/(2.0f);
 	Matrix(1,1) = 1.0f / (float)Native.GetHeight();//1.0f/(2.0f * Native.GetAspectRatio());
-/*	
-	// Draw Something //
-	{
-		ViewMatrix = Matrix;		
 
-		{
-			Matrix4x4 LocalMatrix = Matrix4x4::Identity;
-			LocalMatrix(0,0) = 4;//1.0f/1024.0f;//(12*16)*3;
-			LocalMatrix(1,1) = 4;//1.0f/1024.0f;//(12*16)*3;
-				
-			ViewMatrix *= LocalMatrix;
-		}
-	
-//		Default->Bind( TextureShader );
-//		Default->UniformMatrix4x4( 0, ViewMatrix );
-//		Default->UniformColor( 1, GEL_RGB_WHITE ); // GlobalColor //
-//		Default->Uniform1i( 2, 0 );	// TexImage0 //
-//		Default->BindUniforms();
-//		Texture::Bind( Texas, 0 );
-//		Default->Attrib( 0, Verts );
-//		Default->Attrib( 1, UVs );
-//		Default->DrawArrays( GEL_TRIANGLE_STRIP, 4 );
-		
-		Render::EnableAlphaBlending();
-
-		Project->Draw( ViewMatrix );
-
-//		Render::EnablePremultipliedAlphaBlending();
-//		skeleton->draw( ViewMatrix );
-
-		Render::DisableBlending();
-	}
-	
-	RT_Main->UnBind();
-	
-	// Blur Pass //
-	{
-		RT_Blur[0]->Bind();
-		BlurShader->Bind( 0 );
-		BlurShader->UniformMatrix4x4( 0, Matrix4x4::Identity );
-		BlurShader->Uniform1i( 2, 0 ); // TexImage0 //
-		BlurShader->BindUniforms();
-		RT_Main->BindAsTexture();
-		BlurShader->Attrib( 0, Verts );
-		BlurShader->Attrib( 1, UVs );
-		BlurShader->DrawArrays( GEL_TRIANGLE_STRIP, 4 );
-
-		RT_Blur[1]->Bind();
-		BlurShader->Bind( 1 );
-		BlurShader->UniformMatrix4x4( 0, Matrix4x4::Identity );
-		BlurShader->Uniform1i( 2, 0 ); // TexImage0 //
-		BlurShader->BindUniforms();
-		RT_Blur[0]->BindAsTexture();
-		BlurShader->Attrib( 0, Verts );
-		BlurShader->Attrib( 1, UVs );
-		BlurShader->DrawArrays( GEL_TRIANGLE_STRIP, 4 );
-
-		RT_Blur[1]->UnBind();
-	}
-	
-	// Draw Buffer to Screen //
-	{
-		Default->Bind( TextureShader );
-		Default->UniformMatrix4x4( 0, Matrix4x4::Identity );
-		Default->UniformColor( 1, GEL_RGB_WHITE ); // GlobalColor //
-		Default->Uniform1i( 2, 0 ); // TexImage0 //
-		Default->BindUniforms();
-		RT_Main->BindAsTexture();
-		Default->Attrib( 0, Verts );
-		Default->Attrib( 1, UVs );
-		Default->DrawArrays( GEL_TRIANGLE_STRIP, 4 );
-
-//		Render::EnableAlphaBlending();
-//		{
-//			cUberShader* Sh = PPShader;
-//			PPShader->Bind();
-//			
-////			cUberShader* Sh = Default;
-////			Default->Bind( TextureShader );
-//	
-//			Sh->UniformMatrix4x4( 0, Matrix4x4::Identity );
-//			Sh->UniformColor( 1, GEL_RGB_WHITE ); // GlobalColor //
-//			Sh->Uniform1i( 2, 0 ); // TexImage0 //
-//			Sh->Uniform2f( 3, 1.0f / Native.GetAspectRatio(), 1.0f ); // AspectScalar //
-//			Sh->BindUniforms();
-//			RT_Blur[1]->BindAsTexture();
-//			Sh->Attrib( 0, Verts );
-//			Sh->Attrib( 1, UVs );
-//			Sh->DrawArrays( GEL_TRIANGLE_STRIP, 4 );
-//		}		
-//		Render::DisableBlending();
-	}
-*/	
 	// Draw Text //
 	{
 		ViewMatrix = Matrix;
@@ -250,6 +155,4 @@ void cApp::Draw( Screen::cNative& Native ) {
 	Font->printf( Vector3D(0,0,0), 32.0f, GEL_ALIGN_MIDDLE_CENTER, "I want bacon" );
 
 }
-// - ------------------------------------------------------------------------------------------ - //
-#endif // PRODUCT_CLIENT //
 // - ------------------------------------------------------------------------------------------ - //
