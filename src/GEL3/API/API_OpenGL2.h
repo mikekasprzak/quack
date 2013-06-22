631c980771d23b66d37845f704211c419fa21469
sykhronics <sykhronics@localhost>
2013-06-22 00:36:29 -0600

    GEL3 Started.

---
+++ src/GEL3/API/API_OpenGL2.h
@@ -0,0 +1,19 @@
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __GEL_API_OPENGL2_H__
+#define __GEL_API_OPENGL2_H__
+// - ------------------------------------------------------------------------------------------ - //
+// TODO: Upgrade me to GLEW //
+#include <External/GLEE/GLee.h>
+
+#ifdef _WIN32
+	// Hide some Warnings on Windows //
+	#undef GL_LINES_ADJACENCY_EXT
+	#undef GL_LINE_STRIP_ADJACENCY_EXT
+	#undef GL_TRIANGLES_ADJACENCY_EXT
+	#undef GL_TRIANGLE_STRIP_ADJACENCY_EXT
+#endif // _WIN32
+
+#include <GL/gl.h>
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __GEL_API_OPENGL2_H__ //
+// - ------------------------------------------------------------------------------------------ - //
