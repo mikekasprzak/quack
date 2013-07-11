f968cf0b0bffb79f97391037fd561244f955962f
sykhronics <sykhronics@localhost>
2013-07-11 13:11:44 -0600

    Got the generated file loading inside the android make. Now to use the results.

--- src/RunnerSDLAndroid/jni/src/Android.mk
+++ src/RunnerSDLAndroid/jni/src/Android.mk
@@ -8,6 +8,9 @@ SDL_PATH := ../SDL
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
+include ../../output/sku_client-makefile_android/Quack.so.mk
+# TODO: figure out how to add/use defines
+
 # Add your application source files here...
 LOCAL_SRC_FILES := \
 	$(SDL_PATH)/src/main/android/SDL_android_main.cpp \
