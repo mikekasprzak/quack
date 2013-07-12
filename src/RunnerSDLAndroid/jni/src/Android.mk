1b0ed734d3e803739fa0e672b6d0d6f47049e66e
sykhronics <sykhronics@localhost>
2013-07-11 20:53:56 -0600

    Merged the android generator in to makefile_android. More instructions (running stopping).

--- src/RunnerSDLAndroid/jni/src/Android.mk
+++ src/RunnerSDLAndroid/jni/src/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE := main
 SDL_PATH := ../SDL2
 
 # Load our generated list of things #
-include ../../obj/sku_client-makefile_android/Android.mk
+include ../../output/sku_client-makefile_android/Android.mk
 
 _FLAGS			:=	$(addprefix -D,$(DEFINES))
 
