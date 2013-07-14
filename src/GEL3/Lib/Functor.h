91175e0e20b2de4324dc0dfad3fb0f42212c9d25
sykhronics <sykhronics@localhost>
2013-07-14 01:49:10 -0600

    Did some work on my Functor and added comments to both the Functor and the Signal.

--- src/GEL3/Lib/Functor.h
+++ src/GEL3/Lib/Functor.h
@@ -1,19 +1,42 @@
 // - ------------------------------------------------------------------------------------------ - //
+// My implementation of Functors. AKA: Asynchronous functions. Functions run on another thread. //
+// The syntax is a compromise because I can't rely on C++11 Lambdas. //
+// - ------------------------------------------------------------------------------------------ - //
+// Usage:
+//
+// // Create a struct/class with a () operator //
+// struct MyFunctor {
+// 	 void operator()( ) {
+//     // This will be called in a thread //
+//   }
+// };
+//
+// Functor<MyFunctor> Func; 		// Instance the Functor and Call it //
+// Func.join();						// Wait for the Thread to finish //
+//
+// DFunctor<MyFunctor> Delayed;		// Instance the Functor but DO NOT CALL! //
+// Delayed();						// Call/Start the Functor //
+// Delayed.join();					// Wait for the Thread to finish //
+//
+// TODO: Make this support standard C functions. void Blah() { }
+// - ------------------------------------------------------------------------------------------ - //
 #ifndef __GEL_LIB_FUNCTOR_H__
 #define __GEL_LIB_FUNCTOR_H__
 // - ------------------------------------------------------------------------------------------ - //
 #include <External/TinyThread/tinythread.h>
 // - ------------------------------------------------------------------------------------------ - //
+// Internal Functor Type used by Functor and DFunctor //
+// - ------------------------------------------------------------------------------------------ - //
 template<class Type>
-class Functor: public Type {
+class _Functor: public Type {
 protected:
-	static void StaticFunction( Functor* th ) {
+	static void StaticFunction( _Functor* th ) {
 		th->operator()( );
 	}
 public:
 	typedef void (*FVoidPtr)(void*);
 	
-	inline Functor* GetThis() {
+	inline _Functor* GetThis() {
 		return this;
 	}
 	
@@ -35,12 +58,12 @@ public: // Threading Extensions. Need to manage the thread pointers yourself. //
 // A variation that has an internal Thread and starts automatically //
 // - ------------------------------------------------------------------------------------------ - //
 template<class Type>
-class TFunctor: public Functor<Type> {
+class Functor: public _Functor<Type> {
 protected:
 	tthread::thread Thread;
 public:
-	inline TFunctor() :
-		Thread( Functor<Type>::GetFunc(), this )
+	inline Functor() :
+		Thread( _Functor<Type>::GetFunc(), this )
 	{
 	}
 	
@@ -51,6 +74,45 @@ public:
 	// Wait for Thread to finish //
 	inline void join() {
 		Thread.join();
+	}
+};
+// - ------------------------------------------------------------------------------------------ - //
+// A variation that doesn't start automatically //
+// - ------------------------------------------------------------------------------------------ - //
+template<class Type>
+class DFunctor: public _Functor<Type> {
+protected:
+	tthread::thread* Thread;
+public:
+	inline DFunctor() :
+		Thread( 0 )
+	{
+	}
+	
+	inline ~DFunctor() {
+		if ( Thread ) {
+			delete Thread;
+			//delete_thread( Thread );	// TODO: Try this //
+		}
+	}
+
+	inline void operator()( ) {
+		// NOTE: Only the first call will work //
+		if ( !Thread ) {
+			Thread = new tthread::thread( _Functor<Type>::GetFunc(), this );
+			//Thread = new_thread();	// TODO: Try this //
+		}
+	}
+	
+	inline tthread::thread* GetThread() {
+		return Thread;
+	}
+	
+	// Wait for Thread to finish //
+	inline void join() {
+		if ( Thread ) {
+			Thread->join();
+		}
 	}
 };
 // - ------------------------------------------------------------------------------------------ - //
--- src/GEL3/Lib/Signal.h
+++ src/GEL3/Lib/Signal.h
@@ -4,6 +4,26 @@
 // Does not do: Slot Ordering, Arguments, Chaining Arguments/Returns, Blocking, Scoped Connections
 // Reference: http://www.boost.org/doc/libs/1_52_0/doc/html/signals/tutorial.html
 // - ------------------------------------------------------------------------------------------ - //
+// Usage: 
+//   Signal MySignal;					// Create a Signal //
+//   MySignal.Connect( MyFunc );		// Attach a function //
+//   MySignal.Connect( MyOtherFunc );	// Attach another function //
+//   MySignal();						// Call all of the functions (in Linear/FIFO order) //
+//   MySignal( (void*)256 );			// A void* is passed to all functions //
+//
+// void MyFunc( void* UserDataPtr ) {
+//   // This is what a function typically looks like //
+// }
+//
+// TODO: Investigate how to make this syntax work:
+// void MyOtherFunc( size_t UserData ) {
+//   // The point of this version is to use another type of the same width as a void*. //
+//   // Another useful case would be to pass an int* or OtherType* instead of a void*. //
+// }
+//
+// It may simply be a matter of doing (or not casting at all):
+//   MySignal.Connect( (FVoidPtr)MyFunc );
+// - ------------------------------------------------------------------------------------------ - //
 #ifndef __GEL_LIB_SIGNAL_H__
 #define __GEL_LIB_SIGNAL_H__
 // - ------------------------------------------------------------------------------------------ - //
