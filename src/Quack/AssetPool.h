4e900c863b2e45347ace6d1f9a1d41acfc121f6c
sykhronics <sykhronics@localhost>
2013-07-23 20:56:27 -0600

    Roughed out the entire Asset Pool.

--- src/Quack/AssetPool.h
+++ src/Quack/AssetPool.h
@@ -9,8 +9,6 @@
 #include <string>
 // - ------------------------------------------------------------------------------------------ - //
 class cAsset {
-public:
-	typedef unsigned int UID;
 protected:
 	DataBlock* Data;
 	std::string FileName;
@@ -36,11 +34,11 @@ public:
 	}
 	
 	inline ~cAsset() {
-		UnLoad();
+		Unload();
 	}
 	
 	inline void Load( const char* _FileName ) {
-		UnLoad();
+		Unload();
 		FileName = _FileName;
 		// Using the Null Terminator version of new_read, so the loaded data can //
 		// safely be used as strings. //
@@ -48,9 +46,10 @@ public:
 		Flags |= AF_LOADED;
 	}
 	
-	inline void UnLoad() {
+	inline void Unload() {
 		if ( Data ) {
 			delete_DataBlock( Data );
+			Data = 0;
 		}		
 	}
 	
@@ -67,7 +66,7 @@ public:
 		}
 	}
 	// Variation of Get. Returns an empty string upon fail (instead of 0) //
-	inline char* GetStr() {
+	inline const char* GetStr() {
 		if ( IsLoaded() ) {
 			return Data->Data;
 		}
@@ -84,15 +83,76 @@ public:
 			return 0;
 		}
 	}
+	
+	inline const char* GetFileName() {
+		return FileName.c_str();
+	}
 };
 // - ------------------------------------------------------------------------------------------ - //
 class cAssetPool {
+public:
+	typedef int UID;
 protected:
 	// TODO: This should use the doubler allocator, as assets are a-plenty //
 	GelArray<cAsset>* Assets;
 	
-	std::map<std::string,cAsset::UID> HashTable;
+	std::map<std::string,UID> HashTable;
 public:
+	inline cAssetPool() :
+		Assets( 0 )
+	{
+		// Add 1 element to the Assets array: A dummy placeholder for Id 0. //
+		pushback_GelArray( &Assets );
+	}
+	
+	inline ~cAssetPool() {
+		if ( Assets ) {
+			for ( st idx = 0; idx < Assets->Size; idx++ ) {
+				Assets->Data[idx].Unload();
+			}
+		}
+	}
+	
+public:
+	inline cAsset& operator[] ( const UID Id ) {
+		return Assets->Data[ Id ];
+	}
+	
+	inline UID Load( const char* _FileName ) {
+		// Step 1: Check Hash Table for a match //
+		std::map<std::string,UID>::iterator Itr = HashTable.find(_FileName);
+		if ( Itr != HashTable.end() ) {
+			VVLog( "Asset \"%s\" found in Pool: %i", _FileName, Itr->second );
+			return Itr->second;
+		}
+		// Step 2: If no match, then do a load //
+		else {
+			// TOOD: Check if file exists (where applicible). Return 0 on fail. //
+			
+			// TODO: Scan a released UID's list before deciding to use the back //
+			UID Id = Assets->Size;
+			pushback_GelArray( &Assets );
+			back_GelArray( Assets )->Load( _FileName );
+			HashTable[_FileName] = Id;
+			return Id;
+		}
+	}
+	
+	inline void Unload( const char* _FileName ) {
+		std::map<std::string,UID>::iterator Itr = HashTable.find(_FileName);
+		if ( Itr != HashTable.end() ) {
+			operator[]( Itr->second ).Unload();
+			HashTable.erase( Itr );
+			// Store this UID in the 'available' table //
+		}
+	}
+	
+	inline void Unload( const UID Id ) {
+		// TODO: Check if unloaded //
+		
+		HashTable.erase( HashTable.find( operator[](Id).GetFileName() ) );
+		operator[](Id).Unload();
+	}
 	
 };
 // - ------------------------------------------------------------------------------------------ - //
