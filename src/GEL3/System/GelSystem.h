b52b70cfea68d983c823a9ef908f4996372f86c2
sykhronics <sykhronics@localhost>
2013-07-27 01:17:35 -0600

    Added something resembling Screen Info to Gel.

--- src/GEL3/System/GelSystem.h
+++ src/GEL3/System/GelSystem.h
@@ -2,13 +2,56 @@
 #ifndef __GEL_SYSTEM_GELSYSTEM_H__
 #define __GEL_SYSTEM_GELSYSTEM_H__
 // - ------------------------------------------------------------------------------------------ - //
+#include <vector>
+// - ------------------------------------------------------------------------------------------ - //
 namespace GelSystem {
 // - ------------------------------------------------------------------------------------------ - //
 void Init();
 // - ------------------------------------------------------------------------------------------ - //
 // System Information Functions //
-bool IsAllScreensRequired();	// True on a Multiscreen devices (3DS, Wii U) //
+inline bool HasScreen() {return true;}	// Does it have a screen (duh)? //
+bool HasFixedSizedScreens();			// True if one or more screens have a fixed size (3DS, WiiU, Android, PS Vita, ...)
+bool HasTV();							// True if one or more screens are TVs (Wii U, PS3, PS4)
+bool AreAllScreensRequired();			// True on a Multiscreen devices (3DS, Wii U) //
+// - ------------------------------------------------------------------------------------------ - //
+class cScreen {
+public:
+	int w, h;
+	int Flags;
 
+	enum /* Screen Flags */ {
+		// A Television attached to a console may be variable, meaning developer has choice of resolution (720p). //
+		// Overscan is a seperate property. The Wii U's Gamepad may be considered a TV without Overscan. EDIT: No. //
+		// There isn't much significance to being a TV, just FYI. A Computer Monitor is a TV. //
+		// A TV is a non-integrated screen (as opposed to an integrated LCD). //
+		SF_VARIABLE			= 0x1,		// This screen can vary in size and shape at runtime.
+		SF_OVERSCAN			= 0x2,		// This screen is affected by overscan (TV's only... typically).
+		SF_TV				= 0x4,		// This screen is a television/monitor attached to a Console/PC.
+		SF_WINDOW			= 0x8,		// This screen is a window (PC, Web).
+		
+		SF_SCREEN_TYPE		= 0xF,		// Screen Types. TV, LCD, etc //
+	};
+public:
+	inline bool IsVariable() const {
+		return Flags & SF_VARIABLE;
+	}
+	inline bool IsOverscan() const {
+		return Flags & SF_OVERSCAN;
+	}
+	inline bool IsTV() const {
+		return Flags & SF_TV;
+	}
+	inline bool IsWindow() const {
+		return Flags & SF_WINDOW;
+	}
+
+public:
+	inline void SetFlags( const int _Flags ) {
+		Flags |= _Flags;
+	}
+};
+// - ------------------------------------------------------------------------------------------ - //
+extern std::vector<cScreen> Screen;
 // - ------------------------------------------------------------------------------------------ - //
 };
 // - ------------------------------------------------------------------------------------------ - //
