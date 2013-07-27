c832596cfc74cbb7d1e2fa967c674fb676b6c90f
sykhronics <sykhronics@localhost>
2013-07-26 23:00:43 -0600

    Some homework on standard Squirrel libraries.

--- src/Quack/QuackVM.cpp
+++ src/Quack/QuackVM.cpp
@@ -34,16 +34,17 @@ void QuackVMInit() {
 	
 	// Load Standard Libraries //
 	Log( "Loading System Libraries..." );
-//	sqstd_register_iolib( vm );			// No FileIO //
-	sqstd_register_bloblib( vm );
-	sqstd_register_mathlib( vm );
-//	sqstd_register_systemlib( vm );		// No System Time //
-	sqstd_register_stringlib( vm );
+//	Log("* IO lib"); sqstd_register_iolib( vm );			// FileIO (dofile, loadfile, writeclosuretofile)
+	Log("* Blob lib"); sqstd_register_bloblib( vm );		// Binary File Blobs (unnecessary?)
+	Log("* Math lib"); sqstd_register_mathlib( vm );		// Math (abs, fabs, sin, cos, log, rand, srand, sqrt, PI, ...)
+//	Log("* System lib"); sqstd_register_systemlib( vm );	// System (clock [time], getenv, remove [file], system [cmd], ...)
+	Log("* String lib"); sqstd_register_stringlib( vm );	// String (format [sprintf], regexp, l/r/strip, split [tokens])
 	
 	// Load My Libraries //
 	Log( "Loading Quack Libraries..." );
+	//Log("* qkJson"); register_qkJson( vm );
 	
-	// 
+	// Load the startup file //
 	Log( "Loading Startup File (???.nut)..." );
 	
 	// Do the Init function //
