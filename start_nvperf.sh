f7ba26d4459c8113235a2cfe12f0867ae20485bf
sykhronics <sykhronics@localhost>
2013-11-14 03:59:59 -0500

    More notes on NVPerf.

--- start_nvperf.sh
+++ start_nvperf.sh
@@ -2,3 +2,4 @@
 adb shell setprop debug.perfhudes 1
 
 # http://forums.ouya.tv/discussion/2855/has-anyone-gotten-tegra-perfhud-es-working
+# http://docs.nvidia.com/tegra/index.html#PerfHUD_Quickstart_Guide.html
