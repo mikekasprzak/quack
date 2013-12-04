6ef002a62dd99fbab66eb862ae088382eda56dd3
sykhronics <sykhronics@localhost>
2013-12-04 17:01:00 -0500

    Added GelModel2D and a test case for loading them. Now to actually fill in the loader code.

---
+++ src/GEL3Graphics/Model2D/GelModel2D_Spine.h
@@ -0,0 +1,70 @@
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __GEL_MODEL2D_GELMODEL2D_SPINE_H__
+#define __GEL_MODEL2D_GELMODEL2D_SPINE_H__
+// - ------------------------------------------------------------------------------------------ - //
+#include <Lib/Lib.h>
+#include <Asset/Asset.h>
+#include <Texture/Texture.h>
+// - ------------------------------------------------------------------------------------------ - //
+#include <spine/spine.h>
+// - ------------------------------------------------------------------------------------------ - //
+class GelModel2D {
+protected:
+	//CPVRTModelPOD* Model;
+	std::vector< GelTexturePool::UID > TexturePage;
+	
+public:
+	inline GelModel2D() //:
+//		Model( 0 )
+	{	
+	}
+	
+public:
+	inline void Load( const char* InFile ) {
+		const char* File = Gel::Search( InFile );
+		_Load( File );
+	}
+
+	inline void _Load( const char* InFile, const bool Smooth = true, const bool PreMultiplyAlpha = true ) {
+		Unload();
+
+		GelAssetPool::UID AssetUID = Gel::AssetPool.Load( InFile );		
+
+		if ( AssetUID ) {
+			GelAsset& Asset = Gel::AssetPool[AssetUID];
+			
+//			Model = new CPVRTModelPOD();
+//			EPVRTError Error = Model->ReadFromMemory( Asset.Get(), Asset.GetSize() );
+//		
+//			if ( Error == PVR_SUCCESS ) {
+//				Log("Nodes: %i  MeshNodes: %i  Meshes: %i  AnimationFrames: %i (Lights: %i  Cameras: %i  Materials: %i  Textures: %i)", 
+//					Model->nNumNode, Model->nNumMeshNode, Model->nNumMesh, Model->nNumFrame,
+//					Model->nNumLight, Model->nNumCamera, Model->nNumMaterial, Model->nNumTexture );
+//					
+//				Log("Verts: %i  Faces: %i  UVW: %i",
+//					Model->pMesh[0].nNumVertex, Model->pMesh[0].nNumFaces, Model->pMesh[0].nNumUVW );
+//					
+//				for ( st32 idx = 0; idx < Model->nNumTexture; idx++ ) {
+//					Log("%i T: %s", idx, Model->pTexture[idx].pszName );
+//					const char* TextureFile = Gel::Search( Model->pTexture[idx].pszName );
+//		
+//					if ( TextureFile ) {
+//						TexturePage.push_back( Gel::TexturePool.Load( TextureFile ) );
+//					}
+//				}
+//			}
+		}
+	}
+	
+	inline void Unload() {
+		TexturePage.clear();		
+				
+//		if ( Model ) {
+//			delete Model;
+//			Model = 0;
+//		}
+	}
+};
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __GEL_MODEL2D_GELMODEL2D_SPINE_H__ //
+// - ------------------------------------------------------------------------------------------ - //
