8a431ab64f39a6b9c58cd85135b6bf84936cb792
sykhronics <sykhronics@localhost>
2013-12-06 14:49:50 -0500

    Asset lists are now automatically refreshed whenever a file search fails.

--- src/GEL3System/Asset/GelSearch.h
+++ src/GEL3System/Asset/GelSearch.h
@@ -21,7 +21,11 @@ public:
 	typedef u32 GelSearchUID;
 protected:
 	typedef std::map<std::string,GelSearchUID> HashType;
+
+	// Directiores to Search for Assets //
+	std::vector<std::string> SearchDir;
 	
+	// The File List //
 	std::vector<std::string> File;
 	HashType Hash;					// TODO: Use a hash_map or unordered_map (C++11) //
 public:	
@@ -34,73 +38,88 @@ public:
 public:	
 	inline void Add( const char* DirName ) {
 		Log( "Adding \"%s\" to Search...", DirName );
+
+		SearchDir.push_back( DirName );
+		BuildFileList();
+	}
+
+	inline void BuildFileList() {
+		Log( "+ Rebuilding File List..." );
 		
-		// Store a copy of the original size //
-		st OriginalSize = File.size();
+		File.clear();
+		Hash.clear();
 		
-		{
-			std::string TheDir;
+		for ( st SearchIndex = 0; SearchIndex < SearchDir.size(); SearchIndex++ ) {
+			const char* DirName = SearchDir[SearchIndex].c_str();
+					
+			// Store a copy of the original size //
+			st OriginalSize = File.size();
+			
+			{
+				std::string TheDir;
 #ifdef USES_ANDROID
-			TheDir += "/mnt/sdcard/work/";
+				TheDir += "/mnt/sdcard/work/";
 #endif // USES_ANDROID //
-			TheDir += DirName;
-			// Create a GelDirectory, and populate it //
-			GelDirectory* Dir = new_GelDirectory( TheDir.c_str() );//DirName );
-	
-			// Copy all file names found, adding them to results and hash tables //
-			for( size_t idx = 0; idx < size_GelDirectory( Dir ); idx++ ) {
-				// Since File is a vector (array), the current size will correctly be //
-				// the index of the next element. Therefor, store a copy as a UID. //
-				//GelSearchUID ThisUID = File.size();
-				
-				std::string FullName = std::string(DirName) + "/" + index_GelDirectory( Dir, idx );;
-				
-				// Add this file to the File list //
-				File.push_back( FullName );
-			}	
-	
-			// We're finished and made our own copy, so throw away the GelDirectory //
-			delete_GelDirectory( Dir );		
-		}
+				TheDir += DirName;
+				// Create a GelDirectory, and populate it //
+				GelDirectory* Dir = new_GelDirectory( TheDir.c_str() );//DirName );
 		
-		// Sort the recently added Files //
-		std::sort( File.begin() + OriginalSize, File.end() );
+				// Copy all file names found, adding them to results and hash tables //
+				for( st idx = 0; idx < size_GelDirectory( Dir ); idx++ ) {
+					// Since File is a vector (array), the current size will correctly be //
+					// the index of the next element. Therefor, store a copy as a UID. //
+					//GelSearchUID ThisUID = File.size();
+					
+					std::string FullName = std::string(DirName) + "/" + index_GelDirectory( Dir, idx );;
+					
+					// Add this file to the File list //
+					File.push_back( FullName );
+				}	
+		
+				// We're finished and made our own copy, so throw away the GelDirectory //
+				delete_GelDirectory( Dir );		
+			}
 			
-		st32 DirNameLength = strlen( DirName );
-
-		// Add the recently added files to the Hash table //
-		for( size_t idx = 0; idx < File.size(); idx++ ) {
-			//std::string Name = File[idx]; 							// Includes DirName //
-			std::string Name = File[idx].substr( DirNameLength + 1 );	// Ignores DirName //
-			
-			// Since a linear search of the entire File list will find the first instance //
-			// of a file, then the hash tables should follow a similar behavior. //
-			
-			st32 DirCount = Gel::String::GetSubDirectoryCount( Name );
-			st32 ExtCount = Gel::String::GetExtensionCount( Name );
-			
-			for ( st Dirs = 0; Dirs < DirCount + 1; Dirs++ ) {
-				for ( st Exts = 0; Exts < ExtCount + 1; Exts++ ) {
-					std::string Pattern = Gel::String::GetSubDirectories( Name, Dirs );
-					if ( Pattern.size() > 0 )
-						Pattern += "/";
-					Pattern += Gel::String::GetBaseName( Name ) + Gel::String::GetExtensions( Name, Exts );
-
-					// Make sure it doesn't exist first //
-					if ( Hash.find( Pattern ) == Hash.end() ) {
-						VVVLog( "> %s", Pattern.c_str() );
-						Hash[ Pattern ] = idx;//ThisUID;
-					}
-					else {
-						VVVLog( "** %s exists. Ignoring...", Pattern.c_str() );
+			// Sort the recently added Files //
+			std::sort( File.begin() + OriginalSize, File.end() );
+				
+			st32 DirNameLength = strlen( DirName );
+	
+			// Add the recently added files to the Hash table //
+			for( st idx = 0; idx < File.size(); idx++ ) {
+				//std::string Name = File[idx]; 							// Includes DirName //
+				std::string Name = File[idx].substr( DirNameLength + 1 );	// Ignores DirName //
+				
+				// Since a linear search of the entire File list will find the first instance //
+				// of a file, then the hash tables should follow a similar behavior. //
+				
+				st32 DirCount = Gel::String::GetSubDirectoryCount( Name );
+				st32 ExtCount = Gel::String::GetExtensionCount( Name );
+				
+				for ( st Dirs = 0; Dirs < DirCount + 1; Dirs++ ) {
+					for ( st Exts = 0; Exts < ExtCount + 1; Exts++ ) {
+						std::string Pattern = Gel::String::GetSubDirectories( Name, Dirs );
+						if ( Pattern.size() > 0 )
+							Pattern += "/";
+						Pattern += Gel::String::GetBaseName( Name ) + Gel::String::GetExtensions( Name, Exts );
+	
+						// Make sure it doesn't exist first //
+						if ( Hash.find( Pattern ) == Hash.end() ) {
+							VVVLog( "> %s", Pattern.c_str() );
+							Hash[ Pattern ] = idx;//ThisUID;
+						}
+						else {
+							VVVLog( "** %s exists. Ignoring...", Pattern.c_str() );
+						}
 					}
 				}
 			}
 		}
+		Log("- Finished Rebuilding File List: %i (%i)", File.size(), Hash.size() );
 	}
 	
 	// Search for a Pattern //
-	inline const char* operator()( const char* Pattern ) {
+	inline const char* operator()( const char* Pattern, const int RecursiveDepth = 0 ) {
 		VLog( "* Searching for %s...", Pattern );
 
 		// Hash Search (i.e. fast) //
@@ -111,12 +130,17 @@ public:
 		}
 		
 		// Linear Search (i.e. slow) //
-		for( size_t idx = 0; idx < File.size(); idx++ ) {
+		for( st idx = 0; idx < File.size(); idx++ ) {
 			if ( File[idx].find( Pattern ) != std::string::npos ) {
 				const char* Name = File[idx].c_str();
 				VLog( "* Found %s! (WARNING: Found via Linear Search)", Name );
 				return Name;
 			}
+		}
+		
+		if ( RecursiveDepth == 0 ) {
+			BuildFileList();
+			return operator()( Pattern, RecursiveDepth+1 );
 		}
 
 		Log( "! %s NOT FOUND!!", Pattern );
