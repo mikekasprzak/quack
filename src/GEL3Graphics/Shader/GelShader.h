4f7be09a17ae12fb0bc99e520e80af42c50f8108
sykhronics <sykhronics@localhost>
2014-01-14 18:33:25 -0500

    Added Shader Pool Stub.

---
+++ src/GEL3Graphics/Shader/GelShader.h
@@ -0,0 +1,58 @@
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __GEL_SHADER_GELSHADER_H__
+#define __GEL_SHADER_GELSHADER_H__
+// - ------------------------------------------------------------------------------------------ - //
+#include <Lib/Lib.h>
+#include <Asset/Asset.h>
+#include <Texture/Texture.h>
+#include <Render/Render.h>
+// - ------------------------------------------------------------------------------------------ - //
+//#include <spine/spine.h>
+// - ------------------------------------------------------------------------------------------ - //
+class GelShader {
+protected:
+	st32 MyID;	// My ID (an Internal Copy) //
+
+	GelAssetPool::UID AssetUID;	
+
+//	spAtlas* Atlas;
+	
+public:
+	inline GelShader( const st32 _MyID ) :
+		MyID( _MyID ),
+		AssetUID( 0 )
+//		Atlas( 0 ),
+	{	
+	}
+	
+public:
+	inline void Load( const char* InFile ) {
+		const char* File = Gel::Search( InFile );
+		_Load( File );
+	}
+
+	inline void _Load( const char* InFile ) {
+		Unload();
+		
+		AssetUID = Gel::AssetPool.Load( InFile );
+
+		if ( AssetUID ) {
+			GelAsset& Asset = Gel::AssetPool[AssetUID];
+
+//			Log("+ spShader_readShader");
+//			Atlas = spAtlas_readAtlas( Asset.Get(), Asset.GetSize(), Gel::String::GetDirectorySlash(InFile).c_str() );
+//			Log("- spAtlas_readAtlas (%x)",Atlas);
+		}
+	}
+	
+	inline void Unload() {		
+//		if ( Shader ) {
+//			spAtlas_dispose(Atlas);
+//			Atlas = 0;
+//		}
+	}
+	
+};
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __GEL_SHADER_GELSHADER_H__ //
+// - ------------------------------------------------------------------------------------------ - //
