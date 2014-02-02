401f4f8edb493b7225b2272db8a109b2e965654d
mike <mike@localhost>
2014-02-02 02:10:53 -0500

    Added a bunch of GelGrid2 code, and some GelSubGrid2 code.

--- src/Quack/NewGrid/GelGrid2.h
+++ src/Quack/NewGrid/GelGrid2.h
@@ -19,7 +19,124 @@ class GelGrid2 {
 public:
 	typedef T Type;
 	
+	inline GelGrid2( const st _w = 0, const st _h = 0 ) :
+		w(_w),
+		h(_h),
+		Data(_w*_h)
+	{
+	}
 	
+	// Fast Version //
+	inline int _Index( const int _x, const int _y ) const {
+		return _x + (_y * w);
+	}
+	// The default behavior is Wrapping //
+	inline int Index( int _x, int _y ) const {
+		while ( _x > w ) { _x -= w; };
+		while ( _x < 0 ) { _x += w; };
+		while ( _y > h ) { _y -= h; };
+		while ( _y < 0 ) { _y += h; };
+		return _Index(_x,_y);
+	}
+
+	// Function Operator -- i.e. Normal Usage //	
+	inline T& operator () ( const st _x, const st _y ) {
+		return Data[ Index(_x,_y) ];
+	}
+	inline const T& operator () ( const st _x, const st _y ) const {
+		return Data[ Index(_x,_y) ];
+	}
+	
+	// Array Operator -- Only if you know the Index //
+	inline T& operator [] ( const st _idx ) {
+		return Data[ _idx ];
+	}
+	inline const T& operator [] ( const st _idx ) const {
+		return Data[ _idx ];
+	}
+	
+	// How many elements //
+	inline st Size() const {
+		return Data.size();
+	}
+	inline st SizeOf() const {
+		return Size() * sizeof(T);
+	}
+	// Same thing (really) //
+	inline st Area() const {
+		return w*h;
+	}
+
+	// Width and Height //
+	inline st32 Width() const {
+		return w;
+	}
+	inline st32 Height() const {
+		return h;
+	}
+	inline st32 HalfWidth() const {
+		return w >> 1;
+	}
+	inline st32 HalfHeight() const {
+		return h >> 1;
+	}
+	
+public:
+	// NOTE: Invalidates Data //
+	inline void Resize( const st _w, const st _h ) {
+		w = _w;
+		h = _h;
+		Data.resize(_w*_h);
+	}
+
+public:
+	// Wrap Around as if the data looped back to the start after the end //
+	inline st LoopIndex( const int _x, const int _y ) const {
+		return abs(_Index(_x,_y)) % Size();
+	}
+	// Align to edges instead of wrapping //
+	inline st SaturateIndex( int _x, int _y ) const {
+		if ( _x >= Width() )
+			_x = Width() - 1;
+		else if ( _x < 0 )
+			_x = 0;
+			
+		if ( _y >= Height() )
+			_y = Height() - 1;
+		else if ( _y < 0 )
+			_y = 0;
+			
+		return _Index( _x, _y );
+	}
+	// Return a specific value if outside //
+	inline st DeadIndex( const int _x, const int _y, const st DeadValue = ST_MAX ) const {
+		if ( _x >= (int)Width() )
+			return DeadValue;
+		else if ( _x < 0 )
+			return DeadValue;
+			
+		if ( _y >= (int)Height() )
+			return DeadValue;
+		else if ( _y < 0 )
+			return DeadValue;
+			
+		return _Index( _x, _y );
+	}
+
+	// Extract X or Y values from an Index //
+	inline int IndexToX( const st _idx ) const {
+		// TODO: Assert > 0 (i.e. ABS) //
+		return _idx % w;
+	}
+	inline int IndexToY( const st _idx ) const {
+		// TODO: Assert > 0 (i.e. ABS) //
+		return _idx / w;
+	}
+
+	
+public:
+	// Generate a Sub Grid //
+	inline GelSubGrid2<T> Sub( const st _x, const st _y, const st _w, const st _h );
 };
 // - ------------------------------------------------------------------------------------------ - //
 
--- src/Quack/NewGrid/GelSubGrid2.h
+++ src/Quack/NewGrid/GelSubGrid2.h
@@ -7,17 +7,104 @@
 template<typename T>
 class GelSubGrid2 {
 	typedef GelSubGrid2 thistype;
+	typedef GelGrid2<T> ParentType;
 
-	st32 w,h;		// Shape //
+	st32 w,h;		// Shape (first, to be consistent with GelGrid2) //
 	st32 x,y;		// Offset //
-	GelGrid2<T>* Parent;
+	ParentType* Parent;
 public:
 	typedef T Type;
 
+	inline GelSubGrid2( ParentType* _Parent, const st _x = 0, const st _y = 0, const st _w = 0, const st _h = 0 ) :
+		w(_w),
+		h(_h),
+		x(_x),
+		y(_y),
+		Parent(_Parent)
+	{
+	}
+	
+	// Fast Version //
+	inline st _Index( const st _x, const st _y ) const {
+		return (x+_x) + ((y+_y) * Parent->w);
+	}
+	// The default behavior is Wrapping //
+	inline st Index( st _x, st _y ) const {
+		while ( _x > w ) { _x -= w; };
+		while ( _x < 0 ) { _x += w; };
+		while ( _y > h ) { _y -= h; };
+		while ( _y < 0 ) { _y += h; };
+		// TODO: Should I wrap to main map too? or Call parent's Index? //
+		return _Index(_x,_y);
+	}
+
+	// Function Operator -- i.e. Normal Usage //	
+	inline T& operator () ( const st _x, const st _y ) {
+		return (*Parent)[ Index(_x,_y) ];
+	}
+	inline const T& operator () ( const st _x, const st _y ) const {
+		return (*Parent)[ Index(_x,_y) ];
+	}
+	
+	// Array Operator -- Only if you know the Index //
+	inline T& operator [] ( const st _idx ) {
+		return (*Parent)[ _idx ];
+	}
+	inline const T& operator [] ( const st _idx ) const {
+		return (*Parent)[ _idx ];
+	}
+	
+	// How many elements //
+	inline st Size() const {
+		return Parent->Size();
+	}
+	inline st SizeOf() const {
+		return Size() * sizeof(T);
+	}
+	// Same thing (really) //
+	inline st Area() const {
+		return w*h;
+	}
+
+	// Width and Height //
+	inline st32 Width() const {
+		return w;
+	}
+	inline st32 Height() const {
+		return h;
+	}
+	inline st32 HalfWidth() const {
+		return w >> 1;
+	}
+	inline st32 HalfHeight() const {
+		return h >> 1;
+	}
+	
+public:
+	// NOTE: DOES NOT Invalidate Data //
+	inline void Resize( const st _w, const st _h ) {
+		w = _w;
+		h = _h;
+	}
+	inline void SetPos( const st _x, const st _y ) {
+		x = _x;
+		y = _y;
+	}
+
+	inline void Set( ParentType* _Parent, const st _x = 0, const st _y = 0, const st _w = 0, const st _h = 0 ) {
+		w = _w;
+		h = _h;
+		x = _x;
+		y = _y;
+		Parent = _Parent;
+	}
 	
 };
 // - ------------------------------------------------------------------------------------------ - //
-
+template<typename T>
+GelSubGrid2<T> GelGrid2<T>::Sub( const st _x, const st _y, const st _w, const st _h ) {
+	return Ret( this, _x, _y, _w, _h );
+}
 // - ------------------------------------------------------------------------------------------ - //
 #endif // __GEL_GRID_GELSUBGRID2_H__ //
 // - ------------------------------------------------------------------------------------------ - //
