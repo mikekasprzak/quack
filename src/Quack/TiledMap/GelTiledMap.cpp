fbd91f2653bc11ceaf2f5be12d3004d7861265be
mike <mike@sykhronics.com>
2014-02-04 02:49:19 -0500

    Now opening the JSON data.

--- src/Quack/TiledMap/GelTiledMap.cpp
+++ src/Quack/TiledMap/GelTiledMap.cpp
@@ -4,9 +4,21 @@
 // - ------------------------------------------------------------------------------------------ - //
 void GelTiledMap::Load( const char* const InFile ) {
 	// Use Asset Pool //
+	GelAssetPool::UID Id = Gel::AssetPool.Load( Gel::Search( InFile ) );
+	
+	// Load JSON //
+	cJSON* Root = cJSON_Parse( Gel::AssetPool[Id].GetStr() );
+	
 	
 	// Process JSON Data //
+//	cJSON* format = cJSON_GetObjectItem(root,"format");
+//	int framerate = cJSON_GetObjectItem(format,"frame rate")->valueint;
+	
+	if ( Root ) {
+		Log("I'm a wirrnerr");	
+	}
 	
 	// Cleanup //
+	cJSON_Delete(Root);
 }
 // - ------------------------------------------------------------------------------------------ - //
