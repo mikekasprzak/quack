e1699fb0f2d15332bddaa1822ee00a15e44058dd
mike <mike@sykhronics.com>
2014-02-07 00:41:00 -0500

    Blob Gen seems to be working now.

--- src/Quack/NewGrid/Grid_BlobExtract.h
+++ src/Quack/NewGrid/Grid_BlobExtract.h
@@ -4,6 +4,7 @@
 // - -------------------------------------------------------------------------------------- - //
 // http://en.wikipedia.org/wiki/Blob_extraction
 // http://robotix.in/tutorials/category/imageprocessing/blob_detection
+// ALT: http://junk.mikekasprzak.com/Research/Blob%20Extraction/
 // - -------------------------------------------------------------------------------------- - //
 template<typename T, typename T2, typename F>
 inline void BlobExtract_from_GelGrid( GelGrid<u16>& Out, const T& In, const T2 Middle, F IndexFunc ) {
@@ -16,29 +17,31 @@ inline void BlobExtract_from_GelGrid( GelGrid<u16>& Out, const T& In, const T2 M
 	// First Pass //
 	for ( int y = 0; y < (int)In.Height()+1; y++ ) {
 		for ( int x = 0; x < (int)In.Width()+1; x++ ) {
-			if ( IndexFunc(In,x,y) >= Middle ) {
+			if ( In[IndexFunc(In,x,y)] >= 1 /*Middle*/ ) {
 				enum { N_Size = 3 };
 				u16 N[N_Size];
 				for ( st idx = 0; idx < N_Size; idx++ ) {
 					N[idx] = BGVal;
 				}
 				
-				if ( IndexFunc(In,x-1,y) >= Middle ) {
-					N[0] = Out(x-1,y);
-				}
-				if ( IndexFunc(In,x,y-1) >= Middle ) {
-					N[1] = Out(x,y-1);
-				}
-				N[2] = Out(x,y);
+//				if ( IndexFunc(In,x-1,y) >= Middle ) {
+				if ( x>0 )
+					N[0] = Out[IndexFunc(Out,x-1,y)];
+//				}
+//				if ( IndexFunc(In,x,y-1) >= Middle ) {
+				if ( y>0 )
+					N[1] = Out[IndexFunc(Out,x,y-1)];
+//				}
+				N[2] = Out[IndexFunc(Out,x,y)];
 
 				// If Neighbours is Empty //
 				if ( AllEq( BGVal, N[0], N[1], N[2] ) ) {
 					Linked.Add(NextLabel);
-					Out(x,y) = NextLabel;
+					Out[IndexFunc(Out,x,y)] = NextLabel;
 					NextLabel++;
 				}
 				else {
-					Out(x,y) = ::Min( N[0], N[1], N[2] );	// Ok, because BGVal is big //
+					Out[IndexFunc(Out,x,y)] = ::Min( N[0], N[1], N[2] );	// Ok, because BGVal is big //
 					
 					// Perform a union on Neighbours //
 					for ( st idx = 0; idx < N_Size; idx++ ) {
@@ -60,8 +63,8 @@ inline void BlobExtract_from_GelGrid( GelGrid<u16>& Out, const T& In, const T2 M
 	// Second Pass //
 	for ( st y = 0; y < In.Height(); y++ ) {
 		for ( st x = 0; x < In.Width(); x++ ) {
-			if ( IndexFunc(In,x,y) >= Middle ) {
-				Out(x,y) = Linked.Find( Out(x,y) );
+			if ( In[IndexFunc(In,x,y)] >= 1/*Middle*/ ) {
+				Out[IndexFunc(Out,x,y)] = Linked.Find( Out[IndexFunc(Out,x,y)] );
 			}
 		}
 	}
