abb5b1ead5f125e83361f826a49c9485e658017d
mike <mike@sykhronics.com>
2014-02-24 18:27:16 -0500

    Simplifying Step and Solve code.

--- src/Quack/Engine/QBodyAABB.h
+++ src/Quack/Engine/QBodyAABB.h
@@ -23,18 +23,57 @@ public:
 	QVec 	Old;
 	QFloat	InvMass;
 
+	QFloat	Friction;		// 0 - Ice; 1 - Super Sticky //
+	QFloat	Restitution;	// Elasticity. 1 is perfectly elastic physics (pool ball). 0 is inelastic (pushing). //
+	QVec 	Accum;			// Accumulator (Impulse Forces) //
+
 public:
 	inline QBodyAABB( const QVec& _Pos, const QVec& _HalfShape, const QFloat& _InvMass = QFloat::One ) :
 		Pos( _Pos ),
 		HalfShape( _HalfShape ),
 		Old( _Pos ),
-		InvMass( _InvMass )
+		InvMass( _InvMass ),
+		Friction( QFloat::One ),
+		Restitution( QFloat::One ),
+		Accum( QVec::Zero )
 	{
 	}
 	
 	inline QRect GetRect() {
 		return QRect( Pos - HalfShape, HalfShape+HalfShape );
 	}
+
+public:
+	// NOTE: This explicitly sets the velocity property. It does not accumulate! //
+	inline void SetVelocity( const QVec& _Velocity ) {
+		Old = Pos - _Velocity;
+	}
+	inline void SetMass( const QFloat& _Mass ) {
+		InvMass = QFloat::One / _Mass;
+	}
+	inline void SetFriction( const QFloat& _Friction ) {
+		Friction = _Friction;
+	}
+	inline void SetRestitution( const QFloat& _Restitution ) {
+		Restitution = _Restitution;
+	}
+
+	inline QVec GetVelocity() const {
+		return Pos - Old; // Old -> Pos, Start -> Dest, A -> B, Dest (B) always goes first //
+	}
+	inline QFloat GetMass() const {
+		return QFloat::One / InvMass;
+	}
+	inline const QFloat& GetFriction() const {
+		return Friction;
+	}
+	inline const QFloat& GetRestitution() const {
+		return Restitution;
+	}
+
+	inline void AddForce( const QVec& Force ) {
+		Accum += Force;
+	}	
 
 public:
 	static QFloat _GetInvMass( const thistype* self ) { return self->GetInvMass(); }
@@ -44,11 +83,12 @@ public:
 
 //	static bool _Step( thistype* self, const QProp& Prop ) { return self->Step( Prop ); }
 	inline bool Step( const QProp& Prop ) {
-		QVec Velocity = Pos - Old;
-		Old = Pos;
-		Pos += Velocity + Prop.Gravity;
-		
-		return true;
+		return DoStep_Body( *this, Prop );
+//		QVec Velocity = Pos - Old;
+//		Old = Pos;
+//		Pos += Velocity + Prop.Gravity;
+//		
+//		return true;
 	}
 };
 // - ------------------------------------------------------------------------------------------ - //
--- src/Quack/Engine/QBodyCapsule.h
+++ src/Quack/Engine/QBodyCapsule.h
@@ -25,6 +25,10 @@ public:
 	QVec 	Old;
 	QFloat	InvMass;
 
+	QFloat	Friction;		// 0 - Ice; 1 - Super Sticky //
+	QFloat	Restitution;	// Elasticity. 1 is perfectly elastic physics (pool ball). 0 is inelastic (pushing). //
+	QVec 	Accum;			// Accumulator (Impulse Forces) //
+
 public:
 	inline QBodyCapsule( const QVec& _Pos, const QFloat& _RadiusA, const QVec& _Line, const QFloat& _RadiusB, const QFloat& _InvMass = QFloat::One ) :
 		Pos( _Pos ),
@@ -32,7 +36,10 @@ public:
 		Line( _Line ),
 		RadiusB( _RadiusB ),
 		Old( _Pos ),
-		InvMass( _InvMass )
+		InvMass( _InvMass ),
+		Friction( QFloat::One ),
+		Restitution( QFloat::One ),
+		Accum( QVec::Zero )
 	{
 	}
 	
@@ -45,6 +52,38 @@ public:
 	}
 
 public:
+	// NOTE: This explicitly sets the velocity property. It does not accumulate! //
+	inline void SetVelocity( const QVec& _Velocity ) {
+		Old = Pos - _Velocity;
+	}
+	inline void SetMass( const QFloat& _Mass ) {
+		InvMass = QFloat::One / _Mass;
+	}
+	inline void SetFriction( const QFloat& _Friction ) {
+		Friction = _Friction;
+	}
+	inline void SetRestitution( const QFloat& _Restitution ) {
+		Restitution = _Restitution;
+	}
+
+	inline QVec GetVelocity() const {
+		return Pos - Old; // Old -> Pos, Start -> Dest, A -> B, Dest (B) always goes first //
+	}
+	inline QFloat GetMass() const {
+		return QFloat::One / InvMass;
+	}
+	inline const QFloat& GetFriction() const {
+		return Friction;
+	}
+	inline const QFloat& GetRestitution() const {
+		return Restitution;
+	}
+
+	inline void AddForce( const QVec& Force ) {
+		Accum += Force;
+	}
+
+public:
 	static QFloat _GetInvMass( const thistype* self ) { return self->GetInvMass(); }
 	inline QFloat GetInvMass() const {
 		return InvMass;
@@ -52,11 +91,12 @@ public:
 
 //	static bool _Step( thistype* self, const QProp& Prop ) { return self->Step( Prop ); }
 	inline bool Step( const QProp& Prop ) {
-		QVec Velocity = Pos - Old;
-		Old = Pos;
-		Pos += Velocity + Prop.Gravity;
-		
-		return true;
+		return DoStep_Body( *this, Prop );
+//		QVec Velocity = Pos - Old;
+//		Old = Pos;
+//		Pos += Velocity + Prop.Gravity;
+//		
+//		return true;
 	}
 };
 // - ------------------------------------------------------------------------------------------ - //
@@ -81,47 +121,51 @@ inline bool Solve_Body( QBodyCapsule& A, QBodyCapsule& B ) {
 }
 // - ------------------------------------------------------------------------------------------ - //
 inline bool Solve_Body( QBodySphere& A, QBodyCapsule& B ) {
-	QFloat MassSum = B.InvMass + A.InvMass;
-	return_if_value( false, MassSum == QFloat::Zero );
+//	QFloat MassSum = B.InvMass + A.InvMass;
+//	return_if_value( false, MassSum == QFloat::Zero );
 
 	QVec BPos = NearestPoint_on_Line(B.Pos,B.Pos+B.Line,A.Pos);
 	QFloat BRadius = B.RadiusA; // FIX ME //
 
-	QFloat RadiusSum = BRadius + A.Radius; // Larger than Magnitude //
+	return FinishSolve_Body( A, B, BPos-A.Pos, BRadius+A.Radius );
 
-	QVec Line = BPos - A.Pos;
-	QFloat Mag = Line.MagnitudeSquared();
-	
-	if ( Mag < (RadiusSum*RadiusSum) ) {
-		Mag = RadiusSum - Line.NormalizeRet();
-		A.Pos -= (Line * Mag) * (A.InvMass / MassSum);
-		B.Pos += (Line * Mag) * (B.InvMass / MassSum);
-		return true;
-	}
-	return false;
+//	QFloat RadiusSum = BRadius + A.Radius; // Larger than Magnitude //
+//
+//	QVec Line = BPos - A.Pos;
+//	QFloat Mag = Line.MagnitudeSquared();
+//	
+//	if ( Mag < (RadiusSum*RadiusSum) ) {
+//		Mag = RadiusSum - Line.NormalizeRet();
+//		A.Pos -= (Line * Mag) * (A.InvMass / MassSum);
+//		B.Pos += (Line * Mag) * (B.InvMass / MassSum);
+//		return true;
+//	}
+//	return false;
 }
 // - ------------------------------------------------------------------------------------------ - //
 inline bool Solve_Body( QBodyAABB& A, QBodyCapsule& B ) {
-	QFloat MassSum = B.InvMass + A.InvMass;
-	return_if_value( false, MassSum == QFloat::Zero );
+//	QFloat MassSum = B.InvMass + A.InvMass;
+//	return_if_value( false, MassSum == QFloat::Zero );
 
 	QVec BPos = NearestPoint_on_Line(B.Pos,B.Pos+B.Line,A.Pos);
 	QFloat BRadius = B.RadiusA; // FIX ME //
 
-	QFloat RadiusSum = BRadius; // Larger than Magnitude //
+//	QFloat RadiusSum = BRadius; // Larger than Magnitude //
 
 	QVec ANearestPoint = A.GetRect().NearestPoint( BPos );
+
+	return FinishSolve_Body( A, B, BPos-ANearestPoint, BRadius );
 	
-	QVec Line = BPos - ANearestPoint;
-	QFloat Mag = Line.MagnitudeSquared();
-	
-	if ( Mag < (RadiusSum*RadiusSum) ) {
-		Mag = RadiusSum - Line.NormalizeRet();
-		A.Pos -= (Line * Mag) * (A.InvMass / MassSum);
-		B.Pos += (Line * Mag) * (B.InvMass / MassSum);
-		return true;
-	}
-	return false;
+//	QVec Line = BPos - ANearestPoint;
+//	QFloat Mag = Line.MagnitudeSquared();
+//	
+//	if ( Mag < (RadiusSum*RadiusSum) ) {
+//		Mag = RadiusSum - Line.NormalizeRet();
+//		A.Pos -= (Line * Mag) * (A.InvMass / MassSum);
+//		B.Pos += (Line * Mag) * (B.InvMass / MassSum);
+//		return true;
+//	}
+//	return false;
 }
 // - ------------------------------------------------------------------------------------------ - //
 }; // namespace QK //
