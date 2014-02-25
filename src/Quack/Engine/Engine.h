061928fb6292e37e1a78e52fe58bff12fdd8380c
mike <mike@sykhronics.com>
2014-02-25 14:30:17 -0500

    Placeholders and notes for Sensors.

--- src/Quack/Engine/Engine.h
+++ src/Quack/Engine/Engine.h
@@ -7,17 +7,28 @@ SQInteger register_qkEngine(HSQUIRRELVM v);
 // - ------------------------------------------------------------------------------------------ - //
 #include "QEngine.h"
 // - ------------------------------------------------------------------------------------------ - //
-// Objects have only one body, but can multiple sensors. Bodies must be convex. //
+// Objects have only one body, but can multiple sensors. //
+// Bodies must be convex. Bodies determine orientation (Flips are in Rendering). //
 #include "QBody.h"
 #include "QBodyAABB.h"
 #include "QBodySphere.h"
 #include "QBodyCapsule.h"
+//#include "QBodyOBB.h"
 // - ------------------------------------------------------------------------------------------ - //
+// Sensors can be fancier shapes than bodies. For information gathering and notification. //
+//#include "QSensorAABB.h"
+//#include "QSensorSphere.h"
+//#include "QSensorCapsule.h"
+//#include "QSensorOBB.h"
+//#include "QSensorPoly.h"
+// - ------------------------------------------------------------------------------------------ - //
+// Will eventually only need a few Object types, but for the sake of dev/debugging we have extras.
 #include "QObjDummy.h"
 #include "QObjBoxy.h"
 #include "QObjBoxyStatic.h"
 #include "QObjBally.h"
 #include "QObjCappy.h"
+#include "QObjCappyStatic.h"
 // - ------------------------------------------------------------------------------------------ - //
 #endif // __QUACK_ENGINE_ENGINE_H__ //
 // - ------------------------------------------------------------------------------------------ - //
