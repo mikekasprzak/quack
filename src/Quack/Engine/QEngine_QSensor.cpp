0b3f146c74c796a278f740c5d82f3edc6de1a7f5
mike <mike@sykhronics.com>
2014-03-12 04:14:33 -0400

    Sensors Work!

--- src/Quack/Engine/QEngine_QSensor.cpp
+++ src/Quack/Engine/QEngine_QSensor.cpp
@@ -9,7 +9,7 @@ using namespace QK;
 bool QK::Sense_Sensor( QObj& ObA,QSensor& A, QObj& ObB,QSensor& B ) {	
 	// If a collision was resolved, return true //
 	if ( A.Type == QS_SPINE_BB ) {
-		if ( B.Type == QS_SPINE_BB ) { return Sense_Sensor( ObA, *(QSensorSpineBB*)A.Data, ObB, *(QSensorSpineBB*)B.Data ); }
+		if ( B.Type == QS_SPINE_BB ) { return Sense_Sensor( ObA,*(QSensorSpineBB*)A.Data, ObB,*(QSensorSpineBB*)B.Data ); }
 	}
 	
 	// QS_NULL type tests will fall through, so no need to check for them //
