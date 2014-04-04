67d75a31f8be6c9489ba4ec1853cfebc2cd1ef4f
mike <mike@localhost>
2014-04-04 00:12:02 -0400

    Added Internal Target code for NativeTarget's and RenderTargets.

---
+++ src/Quack/Render/GelTarget/GelRenderTarget_OpenGL.cpp
@@ -0,0 +1,49 @@
+// - ------------------------------------------------------------------------------------------ - //
+#ifdef USES_OPENGL_2_FAMILY
+// - ------------------------------------------------------------------------------------------ - //
+// NOTE: This uses the old Gel RenderTarget code. That should should be merged/moved here, some day.
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+#include "GelRenderTarget.h"
+#include <Render/RenderTarget_OpenGL_FBO.h>
+// - ------------------------------------------------------------------------------------------ - //
+typedef Gel::cRenderTarget RTType;
+// - ------------------------------------------------------------------------------------------ - //
+GelRenderTarget::GelRenderTarget( const int _w, const int _h, const int Textures, const int DepthBuffers, const int StencilBuffers, const bool UseMRT ) {
+	Data = new RTType(_w,_h,Textures,DepthBuffers,StencilBuffers,UseMRT);
+}
+// - ------------------------------------------------------------------------------------------ - //
+GelRenderTarget::~GelRenderTarget() {
+	if ( Data )
+		delete ((RTType*)Data);
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+int GelRenderTarget::GetWidth() const {
+	return ((RTType*)Data)->Width;
+}
+// - ------------------------------------------------------------------------------------------ - //
+int GelRenderTarget::GetHeight() const {
+	return ((RTType*)Data)->Height;
+}
+// - ------------------------------------------------------------------------------------------ - //
+void GelRenderTarget::_Bind( const int Layer ) const {
+	((RTType*)Data)->_Bind( Layer );
+}
+// - ------------------------------------------------------------------------------------------ - //
+void GelRenderTarget::Viewport( const int x, const int y, const int w, const int h ) const {
+	((RTType*)Data)->Viewport( x,y, w,h );
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+void GelRenderTarget::BindAsTexture( const int Layer, const int TextureUnit ) const {
+	((RTType*)Data)->BindAsTexture( Layer, TextureUnit );
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+#endif // USES_OPENGL_2_FAMILY //
+// - ------------------------------------------------------------------------------------------ - //
