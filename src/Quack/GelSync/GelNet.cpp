63cb0f840281f95caa68a664a3db9d1da2ea397b
mike <mike@sykhronics.com>
2014-05-15 14:22:19 -0400

    Can now cleany wire-up a signal that handles packet responses, and it will smartly stop calling the chain (due to Signal improvements just made)

--- src/Quack/GelSync/GelNet.cpp
+++ src/Quack/GelSync/GelNet.cpp
@@ -6,30 +6,25 @@ int GelNet::ReadChunk( const void* Data, const ENetEvent& Event ) {
 	const GelNetClient* Client = (const GelNetClient*)Event.peer->data;
 
 	//	roundTripTime
+	
+	st Ret = 0;
 
 	switch ( Chunk->Type ) {
 		case Gel::PACKET_TEST: {
 			Log("! I GOT YOUR MESSAGE: \"%s\"", (const char*)Chunk->Data );
+			Ret = true;
 			break;
 		}
-		case Gel::PACKET_SYNC_REQUEST: {
-			GelTime LocalTime;
-			LocalTime = get_ms_GelTime();
-
-			Log("Must Respond... %lli", LocalTime );
-			
-			GelPacket Packet;
-			Packet.Add( Gel::PACKET_SYNC_RESPONSE, &LocalTime, sizeof(LocalTime) );
-
-			Send(Packet,Event.peer,0);
-			
-			break;	
-		}
-		case Gel::PACKET_SYNC_RESPONSE: {
-			GelTime* RemoteTime = (GelTime*)Chunk->Data;
-			Log("Response! %lli", *RemoteTime );
-			break;
-		}
+	}
+	
+	GelPacketData PacketData;
+	PacketData.Chunk = Chunk;
+	PacketData.Net = this;
+	PacketData.Event = &Event;
+	
+	Ret = (st)OnPacket( &PacketData, (void*)Ret );
+	if ( Ret == 0 ) {
+		Log("! PACKET WAS UNHANDLED !");
 	}
 	
 	return Chunk->GetTotalSize();
